14cbe024ad50735d24e0fd50b753a696afa6b990
ARTHUR MARCOS SERPA MARTINS <arthur_m_martins@estudante.sesisenai.org.br>
2025-03-14 07:55:55 -0300

    model

--- model/Panetone.js
+++ model/Panetone.js
@@ -12,5 +12,28 @@ export default class Panetone {
     calcularMassa(){
         return this.densidade * this.calcularVolume
     }
-
+    calcularFarinha(){
+        let quantidadeFarinha = (massa * 0.56)
+        return precoFarinha = quantidadeFarinha * 0.01
+    }
+    calcularFarinha(){
+        let quantidadeManteiga = massa * 0.1
+        return precoManteiga = quantidadeManteiga * 0.07
+    }
+    calcularFarinha(){
+        let quantidadeOliva = massa * 0.04
+        return precoOliva = quantidadeOliva * 2.95
+    }
+    calcularFarinha(){
+        let quantidadeLegumes = Math.ceil(massa  * 0.125)
+        return precoLegumes = quantidadeLegumes * 3.56
+    }
+    calcularFarinha(){
+        let quantidadeSal = massa * 0.004
+        return precoSal = quantidadeSal * 0.004
+    }
+    calcularFarinha(){
+        let quantidadeOvo = Math.ceil(massa * 0.14)
+        return precoOvo = quantidadeOvo * 1.4
+    }
 }
