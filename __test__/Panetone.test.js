058f0b1eabab83954f502da21278c16b43b7c3db
Nicollas Prim <nicollas_prim@estudante.sesisenai.org.br>
2025-03-14 08:21:39 -0300

    finalizando test

--- __test__/Panetone.test.js
+++ __test__/Panetone.test.js
@@ -21,4 +21,34 @@ describe("Testes da classe Panetone", ()=>{
 
         expect(panetone.calcularMassa()).toBeCloseTo(massaEsperada,2)
     })
+    
+    test("Teste da quantidade e preço ingredientes: ", ()=>{
+        
+        const quantidadefarinha = (massa * 0.56)
+        const preçofarinha = quantidadefarinha * 0.01
+
+        const quantidademanteiga = massa * 0.1
+        const preçomanteiga = quantidademanteiga * 0.07
+
+        const quantidadeoliva = massa * 0.04
+        const preçooliva = quantidadeoliva * 2.95
+
+        const quantidadelegumes = Math.ceil(massa  * 0.125)
+        const preçolegumes = quantidadelegumes * 3.56
+
+        const quantidadesal = massa * 0.004
+        const preçosal = quantidadesal * 0.004
+
+        const quantidadeovo = Math.ceil(massa / 56)
+        const preçoovo = quantidadeovo * 1.4
+
+        const panetone = new Panetone(massa, preçofarinha, preçolegumes, preçomanteiga, preçooliva, preçoovo, preçosal, quantidadefarinha, quantidadelegumes, quantidademanteiga, quantidadeoliva, quantidadeovo, quantidadesal)
+
+        expect(panetone.calcularFarinha()).toBeCloseTo(preçofarinha,2)
+        expect(panetone.calcularManteiga()).toBeCloseTo(preçomanteiga,2)
+        expect(panetone.calcularOliva()).toBeCloseTo(preçooliva,2)
+        expect(panetone.calcularLegumes()).toBeCloseTo(preçolegumes,2)
+        expect(panetone.calcularSal()).toBeCloseTo(preçosal,2)
+        expect(panetone.calcularOvo()).toBeCloseTo(preçoovo,2)
+    })
 })
--- model/Panetone.js
+++ model/Panetone.js
@@ -16,23 +16,23 @@ export default class Panetone {
         let quantidadeFarinha = (massa * 0.56)
         return precoFarinha = quantidadeFarinha * 0.01
     }
-    calcularFarinha(){
+    calcularManteiga(){
         let quantidadeManteiga = massa * 0.1
         return precoManteiga = quantidadeManteiga * 0.07
     }
-    calcularFarinha(){
+    calcularOliva(){
         let quantidadeOliva = massa * 0.04
         return precoOliva = quantidadeOliva * 2.95
     }
-    calcularFarinha(){
+    calcularLegumes(){
         let quantidadeLegumes = Math.ceil(massa  * 0.125)
         return precoLegumes = quantidadeLegumes * 3.56
     }
-    calcularFarinha(){
+    calcularSal(){
         let quantidadeSal = massa * 0.004
         return precoSal = quantidadeSal * 0.004
     }
-    calcularFarinha(){
+    calcularOvo(){
         let quantidadeOvo = Math.ceil(massa * 0.14)
         return precoOvo = quantidadeOvo * 1.4
     }
